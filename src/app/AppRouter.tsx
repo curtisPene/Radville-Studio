8a6b0d1e3a6050be5fbda8eb0070c9410f5e0306
Curtis Pene <curtispene92@gmail.com>
2025-11-19 01:18:54 +1200

    Update routing and carousel exports

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootLayout } from "./RootLayout";
 import { CarouselPage } from "@/features/carousel/pages/CarouselPage";
 import { TestPage } from "@/features/carousel/pages/TestPage";
-import { TestPage2 } from "@/features/carousel/pages/TestPage2";
 
 export const AppRouter = () => {
   const router = createBrowserRouter([
